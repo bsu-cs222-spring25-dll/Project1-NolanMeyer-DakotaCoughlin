e0b93300d3dfeefa49ebe3cf90e58bc588016db0
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2025-02-13 11:16:41 -0500

    Added resources folder

--- src/test/java/edu/bsu/cs/RevisionFormatterTest.java
+++ src/test/java/edu/bsu/cs/RevisionFormatterTest.java
@@ -2,6 +2,9 @@ package edu.bsu.cs;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RevisionFormatterTest {
@@ -12,6 +15,15 @@ public class RevisionFormatterTest {
         Revision revisionTest = new Revision("Miklogfeather","2023-09-07T18:34:43Z");
         String formattedOutput = revisionFormatter.formatOutput(revisionTest);
         assertEquals("2023-09-07T18:34:43Z  Miklogfeather\n",formattedOutput);
+    }
+
+    @Test
+    public void printRevisionListTest(){
+        InputStream sampleFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("sample.json");
+        RevisionParser parser = new RevisionParser(new RevisionInputStream(sampleFile));
+        List<Revision> revisionList = parser.parse();
+        assertEquals("yes","yes");
+
     }
 
 }
